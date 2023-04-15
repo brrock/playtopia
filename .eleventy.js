8499445ae3dda566019482a26a0fb35887e754e6
Benjy <brduck@duck.com>
2023-04-15 10:21:59 +0100

    Game x v1.0

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('./src/style.css');
   eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/admin/config.yml');  
-
+  eleventyConfig.addPassthroughCopy('./src/play/*/*.js');
+  eleventyConfig.addPassthroughCopy('./src/play/*/*.css');  
+  eleventyConfig.addPassthroughCopy('./src/play/4245/imgs/*.png');
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
